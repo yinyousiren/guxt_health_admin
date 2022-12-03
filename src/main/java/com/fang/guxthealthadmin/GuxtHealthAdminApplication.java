27280b790f8fa9ee2a6d35e8afa16a842121cb54
yinyousiren <1806813650@qq.com>
2022-12-03 13:55:21 +0800

    Initial commit

---
+++ src/main/java/com/fang/guxthealthadmin/GuxtHealthAdminApplication.java
@@ -0,0 +1,13 @@
+package com.fang.guxthealthadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuxtHealthAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuxtHealthAdminApplication.class, args);
+    }
+
+}
